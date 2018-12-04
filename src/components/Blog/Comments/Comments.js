8b2b0f80242892201c6aa5e4121e8c07a408b8a7
Alek Mieczkowski <alekmieczkowski@gmail.com>
2018-12-04 00:58:09 -0500

    Fixed issue with comment button not showing proper text

--- src/components/Blog/Comments/Comments.js
+++ src/components/Blog/Comments/Comments.js
@@ -15,22 +15,36 @@ class Comments extends Component {
 
     render(){
 
+        //render out comments
         let comments = this.props.data.map((comment) => {
             return <Comment key={comment.comment_id} data={comment} />;
         });
 
+        //save all comments - first one
         let allComments = null;
-        if(comments.length > 1 && this.state.showComments){
-            allComments = comments.splice(1);
+        if(comments.length > 1){
+            allComments = comments;
+            allComments = allComments.splice(1);
         }
 
+        //change text on button
+        let  commentText = "No Comments";
+        
+        if(allComments !== null){
+
+            let grammar = "Comment";
+            if(allComments.length > 1){
+                grammar = "Comments";
+            }
+            commentText = this.state.showComments ?  "Hide " + allComments.length + " " + grammar : "Show " + allComments.length + " "+grammar;
+            
+        }
 
         return(
             <div className={classes.container}>
                 {comments[0]}
-                {allComments}
-                <div className={classes.showComments} onClick={this._toggleComments}> { comments.length > 1 ? "Show " + (comments.length - 1) + " comments" : "No Comments"}</div>
-                
+                {this.state.showComments ? allComments : null}
+                <div className={classes.showComments} onClick={this._toggleComments}> {commentText}</div>               
             </div>
 
         );
