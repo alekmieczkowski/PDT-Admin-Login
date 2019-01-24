dcfafcef45dfaeff489881b289f1049eddbf5258
Alek Mieczkowski <alekmieczkowski@gmail.com>
2019-01-23 19:05:07 -0500

    Added Officers section to directory

--- src/containers/Directory/Directory.js
+++ src/containers/Directory/Directory.js
@@ -6,14 +6,49 @@ import UserDirectory from '../../components/Directory/Directory';
 
 class Directory extends Component {
 
+    state={
+        users: [],
+        officers: [],
+    }
+
+    componentDidMount(){
+        let usersArr = [];
+        let officerArr = [];
+        //sort users by officers or members
+        this.props.users.map(user =>{
+
+            
+            if(user.positions.length > 0){
+                //if user has at least 1 position
+                officerArr.push(user);
+            }
+            else{
+                //if user has no positions
+                usersArr.push(user);
+            }
+            return null;
+        });
+        //save to state
+        this.setState({users: usersArr, officers: officerArr});
+    }
+
     render(){
+
 
         return(
             <div className={classes.container}>
-                <div className={classes.header}>
-                    Active Brothers
+                <div className={classes.sectionContainer}>
+                    <div className={classes.header}>
+                        Officers
+                    </div>
+                    <UserDirectory users={this.state.officers}/>
                 </div>
-                <UserDirectory users={this.props.users}/>
+                <div className={classes.sectionContainer}>
+                    <div className={classes.header}>
+                        Members
+                    </div>
+                    <UserDirectory users={this.state.users}/>
+                </div>
             </div>
         );
 
